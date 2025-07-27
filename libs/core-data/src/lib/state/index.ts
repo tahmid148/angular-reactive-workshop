39b2ec8b9a36e3433f79c6149e2a2c23296194bf
tahmid <tahmidrahman2812@outlook.com>
2025-07-27 11:58:19 +0100

    feat: integrated projects into application state

--- libs/core-data/src/lib/state/index.ts
+++ libs/core-data/src/lib/state/index.ts
@@ -1,13 +1,17 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromCustomers from './customers/customers.reducer';
+import * as fromProjects from './projects/projects.reducer';
 
+// Update application state to include projects
 export interface AppState {
   customers: fromCustomers.CustomersState
+  projects: fromProjects.ProjectsState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  customers: fromCustomers.customersReducer
+  customers: fromCustomers.customersReducer,
+  projects: fromProjects.projectsReducer
 };
 
 // -------------------------------------------------------------------
